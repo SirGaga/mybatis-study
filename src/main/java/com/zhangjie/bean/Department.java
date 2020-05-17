c71536489192510a52e29ec2578f289484fac9fb
SirGaga <214151436@qq.com>
2020-05-17 23:12:14 +0800

    day4

--- src/main/java/com/zhangjie/bean/Department.java
+++ src/main/java/com/zhangjie/bean/Department.java
@@ -1,8 +1,19 @@
 package com.zhangjie.bean;
 
+import java.util.List;
+
 public class Department {
     private Integer id;
     private String departmentName;
+    private List<Employee> employees;
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
 
     public Integer getId() {
         return id;
@@ -25,6 +36,7 @@ public class Department {
         return "Department{" +
                 "id=" + id +
                 ", departmentName='" + departmentName + '\'' +
+                ", employees=" + employees +
                 '}';
     }
 }
